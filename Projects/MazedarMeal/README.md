b0561d22c20491da6bd35d2e88f1869a9e62e50f
Muhammad Awais <60058402+AwaisMehnga@users.noreply.github.com>
2024-01-18 23:35:20 +0500

    read me

--- Projects/MazedarMeal/README.md
+++ Projects/MazedarMeal/README.md
@@ -1,8 +1,10 @@
-# React + Vite
+# React
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+These are react projects that I made during the leaning process of react.
+<br>
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+<h2>Mazedar Meal</h2>
+<br>
+A project for fetching data from food api and show recipes.
+<br> 
+In this project I learned about api data fetching in react.
--- Projects/MazedarMeal/src/components/navbar.jsx
+++ Projects/MazedarMeal/src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 export default function Navbar(){
   const[isNavOpen, setisNavOpen]= useState(false)
   return(<div className="navbar">
-  <a href="https://github.com/AwaisMehnga">GitHub</a>
+  <a href="https://github.com/AwaisMehnga/React/tree/main/Projects/MazedarMeal">GitHub</a>
   <a href="https://www.linkedin.com/in/awaismehnga/">LinkedIn</a>
   <a href="https://www.themealdb.com/">MealDB</a>
   <a href="#">Support Me</a>
@@ -12,7 +12,7 @@ export default function Navbar(){
     (<span>X</span> ) : (<span>&#9776;</span> )}
   </div>
   {(isNavOpen)&&<div  className="navbar-responsive">
-  <a href="https://github.com/AwaisMehnga">GitHub</a>
+  <a href="https://github.com/AwaisMehnga/React/tree/main/Projects/MazedarMeal">GitHub</a>
   <a href="https://www.linkedin.com/in/awaismehnga/">LinkedIn</a>
   <a href="https://www.themealdb.com/">MealDB</a>
   <a href="#">Support Me</a>
